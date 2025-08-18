10a3047257570729c868e4afd50a009dd1d56526
zotel1 <zotelsigel@gmail.com>
2025-08-18 20:28:20 -0300

    1. Bucle exterior: i toma valores 1, 2, 4, 8, ... → log₂(n) valores 2. Para cada valor de i, el bucle interior ejecuta i veces 3. Total de iteraciones: 1 + 2 + 4 + 8 + ... ≈ 2n 4. Total: O(n)

---
+++ EjercicioDos.java
@@ -0,0 +1,15 @@
+public class EjercicioDos {
+
+    public static void misterio2(int n){
+        for (int i = 1; i < n; i *= 2){ // Paso 1: ¿Cuantas veces?
+            for (int j = 0; j < i; j++) { // Paso 2: ¿Cuantas veces?
+                System.out.println(j); // Paso 3: ¿Que complejidad?
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        misterio2(10);
+    }
+    
+}
