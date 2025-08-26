ad4b90d8356f09bba300e56044e650023dd33af2
zotel1 <zotelsigel@gmail.com>
2025-08-26 15:56:17 -0300

    Bloquea un objeto/monitor para que solo un hilo ejecute el bloque o método a la vez

---
+++ sincronizacion/Synchronized.java
@@ -0,0 +1,34 @@
+package sincronizacion;
+
+public class Synchronized {
+    public synchronized void metodoSeguro() {
+        System.out.println("Método sincronizado en ejecución: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000); // Simula trabajo
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+}
+    }
+
+    public static void main(String[] args) {
+        Synchronized obj = new Synchronized();
+
+        Thread hilo1 = new Thread(() -> obj.metodoSeguro());
+        Thread hilo2 = new Thread(() -> obj.metodoSeguro());
+
+        hilo1.start();
+        System.out.println("Estado del hilo1 después de start(): " + hilo1.getState());
+        hilo2.start();
+        System.out.println("Estado del hilo2 después de start(): " + hilo2.getState());
+
+        try {
+            hilo1.join();
+            hilo2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hilos terminados.");
+    }
+}
+
