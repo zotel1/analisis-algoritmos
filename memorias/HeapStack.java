b7f72cdf57434abf862bd7a5b2ebce92f5778792
zotel1 <zotelsigel@gmail.com>
2025-08-29 21:04:09 -0300

    En este ejemplo podemos ver que los valores Integer por ejemplo se comparan en este caso con sus posiciones en memoria y no con su contenido en si

---
+++ memorias/HeapStack.java
@@ -0,0 +1,30 @@
+package memorias;
+
+public class HeapStack {
+    
+    public static void main(String[] args) {
+        int a = 500;
+        int b = 500;
+        if (a == b) {
+            System.out.println("a y b son iguales en valor.");
+        } else {
+            System.out.println("a y b no son iguales en valor.");
+        }
+
+        Integer c = 500;
+        Integer d = 500;
+        if (c == d) {
+            System.out.println("c y d son iguales.");
+        } else {
+            System.out.println("c y d no son iguales.");
+        }
+
+        Integer e = new Integer(100);
+        Integer f = new Integer(100);
+        if (e == f) {
+            System.out.println("e y f son iguales.");
+        } else {
+            System.out.println("e y f no son iguales.");
+        }
+    }
+}
